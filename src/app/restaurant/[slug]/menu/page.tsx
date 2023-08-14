fe3258db2fb8b1ee4a2e9cb6dc83a74b21c190a9
lkdbns <luke.debonis@gmail.com>
2023-08-14 14:45:49 -0500

    pages set up

---
+++ src/app/restaurant/[slug]/menu/page.tsx
@@ -0,0 +1,65 @@
+import Link from "../../../../../node_modules/next/link"
+
+
+export default function RestaurantMenu() {
+    return (
+        <main className="bg-gray-100 min-h-screen w-screen">
+            <main className="max-w-screen-2xl m-auto bg-white">
+            {/* NAVBAR */}
+            <nav className="bg-white p-2 flex justify-between">
+                <Link href="/" className="font-bold text-gray-700 text-2xl">
+                    Open Table
+                </Link>
+                <div>
+                <div className="flex">
+                    <button className="bg-blue-400 text-white border p-2 rounded">Signin</button>
+                    <button className="ml-3 border p-2 rounded">Signup</button>
+                </div>
+                </div>
+            </nav>
+            {/* NAVBAR */}
+            {/* HEADER */}
+            <div className="h-96 overflow-hidden">
+                <div className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justified-center items-center">
+                <h1 className="text-7xl text-white capitalize text-shadow text-center">
+                    Milestone Grill (Texas)
+                </h1>
+                </div>
+            </div>
+            {/* HEADER */}
+            {/* DESCRIPTION PORTION*/}
+            <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
+                <div className="bg-white w-[100%] rounded p-3 shadow">
+                {/* RESTAURANT NAV*/}
+                <nav className="flex text-reg border-b pb-2">
+                    <Link href="/restaurant/milstones-grill" className="mr-7">Overview</Link>
+                    <Link href="/restaurant/milstones-grill/menu" className="mr-7">Menu</Link>
+                </nav>
+                {/* RESTAURANT NAV*/}
+                {/* MENU */}
+                <main className="bg-white mgt-10">
+                    <div>
+                    <div className="mt-4 pb-1 mb-1">
+                        <h1 className="font-bold text-4xl">Menu</h1>
+                    </div>
+                    <div className="flex flex-wrap justify-between">
+                        {/* MENU CARD */}
+                        <div className="border-rounded p-3 w-[49%] mb-3">
+                        <h3 className="font-bold text-lg">Surf and Turf</h3>
+                        <p className="font-light mt-1 text-sm">
+                            A well done steak with lobster tail
+                        </p>
+                        <p className="mt-7">$79.99</p>
+                        </div>
+                        {/* MENU CARD */}
+                    </div>
+                    </div>
+                </main>
+                {/* MENU */}
+                </div>
+            </div>
+            {/* DESCRIPTION PORTION*/}
+            </main>
+        </main>
+    )
+}
